315b1c2459a5550faaac3980695250fe35b74d5c
Ankit Shukla <shukla.ankit@protonmail.com>
2021-03-24 09:06:58 -0400

    Angular components 2

--- conFusion/src/app/app.module.ts
+++ conFusion/src/app/app.module.ts
@@ -10,6 +10,11 @@ import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
 import { MatListModule } from '@angular/material/list';
 
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -21,6 +26,9 @@ import { MatListModule } from '@angular/material/list';
     MatToolbarModule,
     FlexLayoutModule,
     MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
